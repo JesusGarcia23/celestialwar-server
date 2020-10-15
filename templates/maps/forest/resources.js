8192c88a5cccd18ee7d946798f0b6988216accfa
= <jacex17@gmail.com>
2020-10-14 19:10:03 -0700

    Add templates folder and maps folder

---
+++ templates/maps/forest/resources.js
@@ -0,0 +1,494 @@
+export let listOfPlatforms = [
+    {
+        name: 'base',
+        type: 'platform',
+        id: 1,
+        width: 100,
+        height: 10,
+        x: 0,
+        y: 20,
+        color: 'brown',
+        xPreference: 'center',
+        yPreference: 'end'
+    },
+    {
+        name: 'collector-base',
+        type: 'platform',
+        id: 2,
+        width: 35,
+        height: 3,
+        x: 0,
+        y: 20,
+        color: 'green',
+        xPreference: 'center',
+        yPreference: null
+    },
+    {
+        name: 'collector-base-separator',
+        type: 'platform',
+        id: 3,
+        width: 1,
+        height: 20,
+        x: 0,
+        y: 0,
+        color: 'green',
+        xPreference: 'center',
+        yPreference: null
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 4,
+        width: 12,
+        height: 3,
+        x:0,
+        y: 75,
+        xPreference: 'start',
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 5,
+        width: 12,
+        height: 3,
+        x:0,
+        y: 75,
+        xPreference: 'end',
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 6,
+        width: 15,
+        height: 3,
+        x:0,
+        y: 75,
+        xPreference: 'center',
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 7,
+        width: 12,
+        height: 3,
+        x: 68,
+        y: 65,
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 8,
+        width: 12,
+        height: 3,
+        x: 22,
+        y: 65,
+        color: 'green'
+    },
+    //THIRD ROW
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 9,
+        width: 5,
+        height: 3,
+        x: 40,
+        y: 60,
+        xPreference: 'start',
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 10,
+        width: 5,
+        height: 3,
+        x: 40,
+        y: 54,
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 11,
+        width: 5,
+        height: 3,
+        x: 55,
+        y: 54,
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 12,
+        width: 5,
+        height: 3,
+        x: 5,
+        y: 60,
+        xPreference: 'end',
+        color: 'green'
+    },
+
+    // FOURTH ROW
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 13,
+        width: 10,
+        height: 3,
+        x: 10,
+        y: 48,
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 14,
+        width: 10,
+        height: 3,
+        x: 80,
+        y: 48,
+        color: 'green'
+    },
+    //FIFTH ROW
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 15,
+        width: 10,
+        height: 3,
+        x: 63,
+        y: 40,
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 16,
+        width: 10,
+        height: 3,
+        x: 28,
+        y: 40,
+        color: 'green'
+    },
+    //SIXTH ROW
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 17,
+        width: 7,
+        height: 3,
+        x: 80,
+        y: 30,
+        xPreference: 'end',
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 18,
+        width: 7,
+        height: 3,
+        x: 80,
+        y: 30,
+        xPreference: 'start',
+        color: 'green'
+    },
+    //SEVENTH ROW
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 19,
+        width: 9,
+        height: 3,
+        x: 14,
+        y: 22,
+        color: 'green'
+    },
+    {
+        name: 'regular-platform',
+        type: 'platform',
+        id: 20,
+        width: 9,
+        height: 3,
+        x: 77,
+        y: 22,
+        color: 'green'
+    },
+    {
+        name: 'sphere container generator 1',
+        type: 'sphere-generator',
+        x: 1.5,
+        y: 55.5,
+    },
+    {
+        name: 'sphere container generator 2',
+        type: 'sphere-generator',
+        x: 96.5,
+        y: 55.5,
+    },
+    {
+        name: 'sphere container generator 3',
+        type: 'sphere-generator',
+        x: 48.5,
+        y: 70.5,
+    },
+    {
+        name: 'sphere container generator 4',
+        type: 'sphere-generator',
+        x: 3,
+        y: 85.5,
+    },
+    {
+        name: 'sphere container generator 4',
+        type: 'sphere-generator',
+        x: 95,
+        y: 85.5,
+    },
+    {
+        name: 'Warrior pedesta; 1',
+        type: 'warrior-pedestal',
+        width: 2,
+        height: 7.5,
+        x: 32,
+        y: 32.5,
+    },
+    {
+        name: 'Warrior pedesta; 1',
+        type: 'warrior-pedestal',
+        width: 2,
+        height: 7.5,
+        x: 67,
+        y: 32.5,
+    },
+    {
+        name: 'sphere-collector',
+        type: 'sphere-collector',
+        x: 35.3,
+        y: 0,
+        width: 14,
+        height: 20,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-collector',
+        type: 'sphere-collector',
+        x: 50.7,
+        y: 0,
+        width: 14,
+        height: 20,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 3,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 4,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 4,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 5,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 6,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 6,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 7,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 8,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 8,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 9,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 10,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 10,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 35.3 + 11,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Angel"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7  + 3,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7  + 4,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7  + 4,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7  + 5,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 6,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 6,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 7,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 8,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 8,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 9,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 10,
+        y: 0 + 10,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 10,
+        y: 0 + 6,
+        radius: 0.7,
+        side: "Demon"
+    },
+    {
+        name: 'sphere-socket',
+        type: 'sphere-socket',
+        x: 50.7 + 11,
+        y: 0 + 8,
+        radius: 0.7,
+        side: "Demon"
+    },
+    
+];
