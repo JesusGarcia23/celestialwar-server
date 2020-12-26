beec38d344ae9abc70f849117e52410765383dfe
= <jacex17@gmail.com>
2020-12-25 16:02:11 -0800

    Add comments to individual Emits

--- sockets/emit/individualEmit.js
+++ sockets/emit/individualEmit.js
@@ -1,13 +1,15 @@
 module.exports = {
 
+    // Check for user logged
     loggedIn(socket, data) {
         socket.emit('loggedIn', {username: data.username, accepted: data.accepted})
     },
 
+    // signs up/creates an user
     newPlayerAccepted(socket, data) {
         socket.emit('newPlayerAccepted', {username: data.username, accepted: data.accepted})
     },
-
+    // sends all rooms to user
     sendAllRooms(socket, rooms) {
         socket.emit('sendAllRooms', rooms);
     },
@@ -15,11 +17,13 @@ module.exports = {
     getInitialRoomData(socket, data) {
         socket.emit('getInitialRoomData', data);
     },
-
+    
+    // redirects user to the room
     sendUserToRoom(socket, roomId) {
         socket.emit('goToRoom', roomId);
     },
 
+    // removes user from the room
     userLeaveRoom(socket) {
         socket.leaves(`room/${roomId}`);
         socket.emit('exitedRoom', true);
