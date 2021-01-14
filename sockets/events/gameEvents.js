ab9bf88d871b817c79a6f024e0de22eed60e77c0
= <jacex17@gmail.com>
2021-01-13 17:21:45 -0800

    Add more utils to room

--- sockets/events/gameEvents.js
+++ sockets/events/gameEvents.js
@@ -1,6 +1,6 @@
 const individualEmit = require('../emit/individualEmit');
 const groupalEmit = require('../emit/groupalEmit');
-const { roomFinder } = require('../../utils/roomUtils');
+const { roomFinder, movePlayer, grabSphere, playerRespawner, playerAttackSystem } = require('../../utils/roomUtils');
 
 module.exports = {
 
@@ -40,34 +40,10 @@ module.exports = {
             let actualRoom = roomFinder(roomId, rooms);
 
             if (actualRoom) {
-                for (let j = 0; j <= actualRoom.gameStatus.players.length - 1; j++) {
 
-                    if (actualRoom.gameStatus.players[j].name === player.name) {
-                        
-                        switch (direction) {
-                            case "LEFT": 
-                                actualRoom.gameStatus.players[j].direction = direction;
-                                actualRoom.gameStatus.players[j].x -= moveAmount;
-                                break;
-                            case "RIGHT":
-                                actualRoom.gameStatus.players[j].direction = direction;
-                                actualRoom.gameStatus.players[j].x += moveAmount;
-                                break;
-                            case "UP":
-                                actualRoom.gameStatus.players[j].onFloor = false;
-                                actualRoom.gameStatus.players[j].y -= moveAmount;
-                                break;
-                            case "DOWN":
-                                actualRoom.gameStatus.players[j].y += moveAmount;
-                            default:
-                                break;
-                        }
-                        break;
-                    }
+                // return room with player positions updated (player moved)
+                updatedActualRoom = movePlayer(actualRoom, player, direction, moveAmount);
 
-                }
-
-                updatedActualRoom = actualRoom;  
                 groupalEmit.updateGameStatus(io, updatedActualRoom);
             }
 
@@ -90,22 +66,14 @@ module.exports = {
 
                 let sphereToGrab = null;
 
-                if (sphereToGrabIndex >= 0) {
-                    sphereToGrab = actualRoom.gameStatus.spheres[sphereToGrabIndex];
+                if (sphereToGrabIndex < 0) {
+                    return;
                 }
 
-                for (let j = 0; j <= actualRoom.gameStatus.players.length - 1; j++) {
+                sphereToGrab = actualRoom.gameStatus.spheres[sphereToGrabIndex];
 
-                    if (actualRoom.gameStatus.players[j].name === player.name && !actualRoom.gameStatus.players[j].sphereGrabbed 
-                        && sphereToGrab && sphereToGrab.grabbedBy === "") {
-                        actualRoom.gameStatus.spheres[sphereToGrabIndex].grabbedBy = player.name;
-                        actualRoom.gameStatus.players[j].sphereGrabbed = true;
-                        break;
-                    }
-
-                }
-
-                updatedActualRoom = actualRoom;  
+                // return room with player and sphere updated properties
+                updatedActualRoom = grabSphere(actualRoom, player, sphereToGrabIndex, sphereToGrab);  
 
                 groupalEmit.updateGameStatus(io, updatedActualRoom);
 
@@ -128,45 +96,17 @@ module.exports = {
     playerAttack (io, socket, rooms) {
         socket.on('playerAttacked', (data) => {
 
-            const {firstPlayer, secondPlayer, roomId, action, points } = data;
-
-            let attackHappened = false;
+            const {firstPlayer, secondPlayer, roomId, action } = data;
 
             let actualRoom = roomFinder(roomId, rooms);
 
             if (actualRoom) {
 
-                let firstPlayerToUpdateIndex = actualRoom.gameStatus.players.findIndex(playerToFind => playerToFind.name === firstPlayer.name);
+                // Returns room with players updated properties (alive)
+                let updatedActualRoom = playerAttackSystem(actualRoom, firstPlayer, secondPlayer, action);  
 
-                let secondPlayerToUpdateIndex = actualRoom.gameStatus.players.findIndex(playerToFind => playerToFind.name === secondPlayer.name);
-
-                if (firstPlayerToUpdateIndex >= 0 && secondPlayerToUpdateIndex >= 0) {
-                        
-                    switch (action) {
-                        case "ATTACK":
-                            // update second player properties
-                            // actualRoom.gameStatus.players[firstPlayerToUpdateIndex]
-
-                            console.log(actualRoom.gameStatus.players[secondPlayerToUpdateIndex])
-
-                            // update second player properties
-                            actualRoom.gameStatus.players[secondPlayerToUpdateIndex].alive = false;
-                            attackHappened = true;
-
-                            if (actualRoom.gameStatus.players[secondPlayerToUpdateIndex].king) {
-                                console.log("KING KILLED!")
-                            }
-
-                            break;
-                        
-                        default:
-                            break;
-                    }
-
-                    if (attackHappened) {
-                        let updatedActualRoom = actualRoom;  
-                        groupalEmit.updateGameStatus(io, updatedActualRoom);
-                    }
+                if (updatedActualRoom && updatedActualRoom.attackHappened) {
+                    groupalEmit.updateGameStatus(io, updatedActualRoom.room);
                 }
 
             }
@@ -180,23 +120,17 @@ module.exports = {
         socket.on('respawnPlayer', (data) => {
             console.log("REQUESTING RESPAWN ", data)
 
-            const { myPlayer, roomId } = data;
+            const { player, roomId } = data;
 
             let actualRoom = roomFinder(roomId, rooms);
 
             if (actualRoom) {
 
-                for (let j = 0; j <= actualRoom.gameStatus.players.length - 1; j++) {
-                    if ((actualRoom.gameStatus.players[j].name === myPlayer.name) && !actualRoom.gameStatus.players[j].alive) {
-                        actualRoom.gameStatus.players[j].x = actualRoom.gameStatus.players[j].deployX;
-                        actualRoom.gameStatus.players[j].y = actualRoom.gameStatus.players[j].deployY;
-                        actualRoom.gameStatus.players[j].alive = true;
-                        break;
-                    }
-                }
+                // return room with player respawn
+                let updatedActualRoom = playerRespawner(player, actualRoom);  
 
-                let updatedActualRoom = actualRoom;  
                 individualEmit.resetRespawnRequest(socket, true);
+
                 groupalEmit.updateGameStatus(io, updatedActualRoom);
             }
 
--- utils/roomUtils.js
+++ utils/roomUtils.js
@@ -9,3 +9,97 @@ export const roomFinder = (roomId, rooms) => {
     }
     return null;
 }
+
+
+export const movePlayer = (room, player, direction, moveAmount) => {
+
+    for (let j = 0; j <= room.gameStatus.players.length - 1; j++) {
+
+        if (room.gameStatus.players[j].name === player.name) {
+            
+            switch (direction) {
+                case "LEFT": 
+                    room.gameStatus.players[j].direction = direction;
+                    room.gameStatus.players[j].x -= moveAmount;
+                    break;
+                case "RIGHT":
+                    room.gameStatus.players[j].direction = direction;
+                    room.gameStatus.players[j].x += moveAmount;
+                    break;
+                case "UP":
+                    room.gameStatus.players[j].onFloor = false;
+                    room.gameStatus.players[j].y -= moveAmount;
+                    break;
+                case "DOWN":
+                    room.gameStatus.players[j].y += moveAmount;
+                default:
+                    break;
+            }
+            break;
+        }
+    }
+
+    return room;
+}
+
+export const grabSphere = (room, player, sphereToGrabIndex, sphereToGrab) => {
+
+    for (let j = 0; j <= room.gameStatus.players.length - 1; j++) {
+
+        if (room.gameStatus.players[j].name === player.name && !room.gameStatus.players[j].sphereGrabbed 
+            && sphereToGrab && sphereToGrab.grabbedBy === "") {
+            room.gameStatus.spheres[sphereToGrabIndex].grabbedBy = player.name;
+            room.gameStatus.players[j].sphereGrabbed = true;
+            break;
+        }
+    }
+
+    return room;
+}
+
+export const playerAttackSystem = (actualRoom, firstPlayer, secondPlayer, action) => {
+
+    let attackHappened = false;
+
+    let firstPlayerToUpdateIndex = actualRoom.gameStatus.players.findIndex(playerToFind => playerToFind.name === firstPlayer.name);
+
+    let secondPlayerToUpdateIndex = actualRoom.gameStatus.players.findIndex(playerToFind => playerToFind.name === secondPlayer.name);
+
+    if (firstPlayerToUpdateIndex >= 0 && secondPlayerToUpdateIndex >= 0) {
+            
+        switch (action) {
+            case "ATTACK":
+
+                console.log(actualRoom.gameStatus.players[secondPlayerToUpdateIndex])
+
+                // update second player properties
+                actualRoom.gameStatus.players[secondPlayerToUpdateIndex].alive = false;
+                attackHappened = true;
+
+                if (actualRoom.gameStatus.players[secondPlayerToUpdateIndex].king) {
+                    console.log("KING KILLED!")
+                }
+
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    return {attackHappened: attackHappened, room: actualRoom};
+}
+
+export const playerRespawner = (room, player) => {
+
+    for (let j = 0; j <= actualRoom.gameStatus.players.length - 1; j++) {
+        if ((room.gameStatus.players[j].name === player.name) && !room.gameStatus.players[j].alive) {
+            room.gameStatus.players[j].x = room.gameStatus.players[j].deployX;
+            room.gameStatus.players[j].y = room.gameStatus.players[j].deployY;
+            room.gameStatus.players[j].alive = true;
+            break;
+        }
+    }
+
+    return room;
+}
